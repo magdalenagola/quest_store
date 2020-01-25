8fe4ee7f1cf46592ca6d49b01e2a367936feb8dd
e-victoria <evictoria@protonmail.com>
2020-01-25 15:19:32 +0100

    Add task for compiling css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require("gulp");
 var server = require("browser-sync").create();
+var concat = require('gulp-concat');
+var csso = require("gulp-csso");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+
+gulp.task("css", function () {
+	return gulp.src(['src/css/*.css', 'src/css/blocks/*.css'])
+		.pipe(csso())
+		.pipe(postcss([
+			autoprefixer()
+		]))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('src/css'));
+});
 
 gulp.task("server", function () {
   server.init({
