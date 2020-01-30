dc9aa88d58dcbbe24dc58bfb33c9fad3b5c98f71
e-victoria <evictoria@protonmail.com>
2020-01-31 00:51:16 +0100

    Change tables in mobile version

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import toggleMenu from './menu.js';
+import expandTableContent from './table_expander.js';
 
 (function() {
 
     toggleMenu();
+    expandTableContent();
 
 })();
---
+++ src/js/table_expander.js
@@ -0,0 +1,16 @@
+export default function expandTableContent() {
+    const transactionItems = document.querySelectorAll('li.transactions__item');
+    const transtactionItemsContent = document.querySelectorAll('.transactions__content-wrapper');
+    const mentorsItems = document.querySelectorAll('li.mentors__item');
+    const mentorsItemsContent = document.querySelectorAll('.mentors__content-wrapper');
+    for (let i = 0; i < transactionItems.length; i++) {
+        transactionItems[i].addEventListener('click', () => {
+            transtactionItemsContent[i].classList.toggle('transactions__content-wrapper--opened');
+        })
+    }
+    for (let i = 1; i < mentorsItems.length; i++) {
+        mentorsItems[i].addEventListener('click', () => {
+            mentorsItemsContent[i].classList.toggle('mentors__content-wrapper--opened');
+        })
+    }
+}
