278a3e5fef20074579532808cd727ce0f14d6d21
e-victoria <evictoria@protonmail.com>
2020-01-31 02:23:17 +0100

    Add buttons to edit and remove mentors

--- src/js/table_expander.js
+++ src/js/table_expander.js
@@ -3,14 +3,19 @@ export default function expandTableContent() {
     const transtactionItemsContent = document.querySelectorAll('.transactions__content-wrapper');
     const mentorsItems = document.querySelectorAll('li.mentors__item');
     const mentorsItemsContent = document.querySelectorAll('.mentors__content-wrapper');
+
     for (let i = 0; i < transactionItems.length; i++) {
         transactionItems[i].addEventListener('click', () => {
             transtactionItemsContent[i].classList.toggle('transactions__content-wrapper--opened');
         })
     }
     for (let i = 1; i < mentorsItems.length; i++) {
-        mentorsItems[i].addEventListener('click', () => {
+        mentorsItems[i].addEventListener('click', (e) => {
+            if(e.toElement.tagName == 'H3') {
             mentorsItemsContent[i].classList.toggle('mentors__content-wrapper--opened');
+            }
         })
     }
+
+    
 }
