b750e9372a0a3b3ee40b74d698fc98405ca7d36e
szczepanTopolski <szczepanPL678@gmail.com>
2020-02-05 15:54:51 +0100

    Add mentor class constructor

--- com/codecool/java/model/Mentor.java
+++ com/codecool/java/model/Mentor.java
@@ -3,6 +3,19 @@ package codecool.java.model;
 import codecool.java.controller.MentorController;
 
 public class Mentor extends User{
+    private int id;
+    private String login;
+    private String password;
+    private String name;
+    private String surname;
+
+    public Mentor(int id,String login, String password,String name,String surname){
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+    }
     @Override
     public void start() {
         MentorController controller = new MentorController();
