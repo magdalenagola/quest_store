bf60831191325d0eab55e3ba0c8c531662970c35
e-victoria <evictoria@protonmail.com>
2020-02-06 21:37:54 +0100

    Add mentorDAO and dbMentorDAO

---
+++ com/codecool/java/dao/DbMentorDAO.java
@@ -0,0 +1,15 @@
+package codecool.java.dao;
+
+import codecool.java.model.BasicConnectionPool;
+
+import java.sql.SQLException;
+
+public class DbMentorDAO implements MentorDAO {
+    private BasicConnectionPool pool;
+
+    public DbMentorDAO() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        this.pool = BasicConnectionPool.create("jdbc:postgresql://ec2-176-34-237-141.eu-west-1.compute.amazonaws.com:5432/dbc5jifafq3j1h?sslmode=require",
+                "utiuhfgjckzuoq", "17954f632e3663cbadb55550dd636f4c3a645ade56c3342ee89f71fc732c9672");
+    }
+}
---
+++ com/codecool/java/dao/MentorDAO.java
@@ -0,0 +1,7 @@
+package codecool.java.dao;
+
+import codecool.java.model.BasicConnectionPool;
+
+public interface MentorDAO extends DAO {
+
+}
