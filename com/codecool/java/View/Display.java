88d154449af74f9ad3c41fcb6ca827a076e0a29a
Magdalena Gola <turcza.magdalena@gmail.com>
2020-02-06 20:23:36 +0100

    Display quests, cards and transactions

--- com/codecool/java/View/Display.java
+++ com/codecool/java/View/Display.java
@@ -7,6 +7,14 @@ public interface Display {
     int getOptionInput(int maxOptionsNumber);
     void displayUser(User user);
     void displayUsers(ArrayList<User> users);
+    void displayQuest(Quest quest);
+    void displayQuests(ArrayList<Quest> quests);
+    void displayCard(Card card);
+    void displayCards(ArrayList<Card> card);
+    void displayCardTransaction(CardTransaction ct);
+    void displayCardTransactions(ArrayList<CardTransaction> cts);
+    void displayQuestTransaction(QuestTransaction qt);
+    void displayQuestTransaction(ArrayList<QuestTransaction> qts);
     void displayMessage(String msg);
     void displayErrorMessage(Exception e);
 }
