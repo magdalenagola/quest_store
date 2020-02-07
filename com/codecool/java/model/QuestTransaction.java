305fba89975bbe21077f5884279e84bf8369130f
e-victoria <evictoria@protonmail.com>
2020-02-08 00:38:57 +0100

    Fix mistake in class name

--- com/codecool/java/model/QuestTransaction.java
+++ com/codecool/java/model/QuestTransaction.java
@@ -2,13 +2,13 @@ package codecool.java.model;
 
 import java.util.Date;
 
-public class CardTransaction extends Transaction {
+public class QuestTransaction extends Transaction {
     private int questId;
     private int userId;
     private Date transactionDate;
     int cost;
 
-    CardTransaction(int questId, int userId, Date transactionDate, int cost) {
+    QuestTransaction(int questId, int userId, Date transactionDate, int cost) {
         this.questId = questId;
         this.userId = userId;
         this.transactionDate = transactionDate;
