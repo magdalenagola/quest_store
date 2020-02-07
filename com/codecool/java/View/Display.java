4be814bd3e702af2c75ed6bac308b71418879959
Magdalena Gola <turcza.magdalena@gmail.com>
2020-02-07 09:56:43 +0100

    Add access modifiers and parameter to display options method

--- com/codecool/java/View/Display.java
+++ com/codecool/java/View/Display.java
@@ -3,18 +3,18 @@ package codecool.java.View;
 import java.util.ArrayList;
 
 public interface Display {
-    void displayOptions(String[]);
-    int getOptionInput(int maxOptionsNumber);
-    void displayUser(User user);
-    void displayUsers(ArrayList<User> users);
-    void displayQuest(Quest quest);
-    void displayQuests(ArrayList<Quest> quests);
-    void displayCard(Card card);
-    void displayCards(ArrayList<Card> card);
-    void displayCardTransaction(CardTransaction ct);
-    void displayCardTransactions(ArrayList<CardTransaction> cts);
-    void displayQuestTransaction(QuestTransaction qt);
-    void displayQuestTransaction(ArrayList<QuestTransaction> qts);
-    void displayMessage(String msg);
-    void displayErrorMessage(Exception e);
+    public void displayOptions(String[] options);
+    public int getOptionInput(int maxOptionsNumber);
+    public void displayUser(User user);
+    public void displayUsers(ArrayList<User> users);
+    public void displayQuest(Quest quest);
+    public void displayQuests(ArrayList<Quest> quests);
+    public void displayCard(Card card);
+    public void displayCards(ArrayList<Card> card);
+    public void displayCardTransaction(CardTransaction ct);
+    public void displayCardTransactions(ArrayList<CardTransaction> cts);
+    public void displayQuestTransaction(QuestTransaction qt);
+    public void displayQuestTransaction(ArrayList<QuestTransaction> qts);
+    public void displayMessage(String msg);
+    public void displayErrorMessage(Exception e);
 }
