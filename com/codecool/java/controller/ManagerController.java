da9f7984406f0fadf965ea6a51454ed1854fc2e7
e-victoria <evictoria@protonmail.com>
2020-02-07 22:08:32 +0100

    Implement methods from managerController and studentController

--- com/codecool/java/controller/ManagerController.java
+++ com/codecool/java/controller/ManagerController.java
@@ -2,6 +2,8 @@ package codecool.java.controller;
 package codecool.java.view;
 
 import codecool.java.dao.DbMentorDAO;
+import codecool.java.dao.MentorDAO;
+import codecool.java.view.terminalView;
 import codecool.java.model.Mentor;
 
 import java.sql.SQLException;
@@ -9,36 +11,93 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ManagerController {
-    DbMentorDAO dbMentorDAO;
-    TerminalView terminalView;
+    private MentorDAO mentorDAO;
+    private TerminalView terminalView;
+
+    public ManagerController() throws SQLException, ClassNotFoundException {
+        mentorDAO = new DbMentorDAO();
+        terminalView = new TerminalView()
+    }
+
     public void run() {
-        String[] options = {"Add new mentor", "Edit mentor's profile", "Show all mentors"};
-        View.displayOptions(options);
-        int userInput = View.getOptionInput(options.length);
+        String[] options = {"Add new mentor", "Edit mentor's profile", "Show all mentors", "Remove mentor", "Activate mentor"};
+        terminalView.displayOptions(options);
+        int userInput = terminalView.getOptionInput(options.length);
         switch(userInput){
             case 1:
-                addNewMentor(mentors);
+                addNewMentor();
                 break;
             case 2:
-                editMentor(mentor);
+                editMentor();
                 break;
             case 3:
                 showAllMentors();
                 break;
+            case 4:
+                disableMentor();
+                break;
+            case 5:
+                activateMentor();
+                break;
         }
     }
 
-    private void showAllMentors() throws SQLException, ClassNotFoundException {
+    private int chooseMentor() {
         List<Mentor> mentors= new ArrayList<>();
-        terminalView = new TerminalView();
-        dbMentorDAO = new DbMentorDAO();
-        mentors = dbMentorDAO.loadAll();
+        mentors = mentorDAO.loadAll();
+
+        terminalView.displayUsers(mentors);
+        return terminalView.getOptionInput(mentors.size()) - 1;
+    }
+
+    private void disableMentor() {
+        List<Mentor> mentors= new ArrayList<>();
+        mentors = mentorDAO.loadAll();
+        Mentor mentor = mentors.get(chooseMentor());
+        mentorDAO.disable(mentor);
+    }
+
+    private void activateMentor() {
+        List<Mentor> mentors= new ArrayList<>();
+        mentors = mentorDAO.loadAll();
+        Mentor mentor = mentors.get(chooseMentor());
+        mentorDAO.activate(mentor);
+    }
+
+    private void showAllMentors() {
+        List<Mentor> mentors= new ArrayList<>();
+        mentors = mentorDAO.loadAll();
         terminalView.displayUsers(mentors);
     }
 
-    private void editMentor(Mentor mentor) {
+    private Mentor editMentor() {
+        String[] mentorDataToEdit = {"Name", "Surname", "Email", "Password", "Primary skill"};
+        List<Mentor> mentors= new ArrayList<>();
+        mentors = mentorDAO.loadAll();
+        Mentor mentor = mentors.get(chooseMentor());
+
+        terminalView.displayOptions(mentorDataToEdit);
+        String[] optionsToChange = terminalView.getOpgetInputstionInput(mentorDataToEdit);
+        mentor.setName(optionsToChange[0]);
+        mentor.setSurname(optionsToChange[1]);
+        mentor.setEmail(optionsToChange[2]);
+        mentor.setPassword(optionsToChange[3]);
+        mentor.setPrimarySkill(optionsToChange[4]);
+
+        mentorDAO.update(mentor);
+
+        terminalView.displayMessage("Successfully edited mentor data!");
+
+        return mentor;
     }
 
     private Mentor addNewMentor() {
+        String[] requiredUserData = {"Name", "Surname", "Email", "Password", "Primary skill"};
+        String[] newData =  terminalView.getInputs(requiredUserData);
+        Mentor newMentor;
+        newMentor = new Mentor(newData[0], newData[1], newData[2], newData[3], newData[4], true);
+        mentorDAO.save(newMentor);
+        terminalView.displayMessage("Successfully added new mentor!");
+        return newMentor;
     }
 }
--- com/codecool/java/controller/StudentController.java
+++ com/codecool/java/controller/StudentController.java
@@ -1,10 +1,34 @@
 package codecool.java.controller;
 
+import codecool.java.dao.TransactionsDAO;
+import codecool.java.model.CardTransaction;
+import codecool.java.model.QuestTransaction;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 public class StudentController {
-    public void run() {
-        String[] options = {"Show all cards", "Show all quests", "Show my transactions", "Buy a card"};
-        View.displayOptions(options);
-        int userInput = View.getOptionInput(options.length);
+    private CardDAO cardDAO;
+    private QuestDAO questDAO;
+    private TransactionsDAO transactionsDAO;
+    private TerminalView terminalView;
+    private List<Transaction> transactions;
+    private List<Card> cards;
+    private List<Quest> quests;
+
+    public StudentController() throws SQLException, ClassNotFoundException {
+        cardDAO = new DbCardDAO();
+        questDAO = new DbQuestDAO();
+        transactionsDAO = new DBtransactionsDAO();
+        terminalView = new TerminalView();
+    }
+
+    public void run(Student student) {
+        String[] options = {"Show all cards", "Show all quests", "Show my transactions", "Buy a card", "Mark quest as achieved"};
+        terminalView.displayOptions(options);
+        int userInput = terminalView.getOptionInput(options.length);
         switch(userInput){
             case 1:
                 showAllCards();
@@ -16,24 +40,60 @@ public class StudentController {
                 showTransactions();
                 break;
             case 4:
-                buyCard(Card card); //TODO
+                buyCard(student);
                 break;
+            case 5:
+                submitQuest(student);
         }
     }
 
     private void showAllCards() {
-
+        cards = getCards();
+        terminalView.displayCards(cards);
     }
 
     private void showAllQuests() {
+        quests = getQuests();
+        terminalView.displayQuests(quests);
+    }
 
+    private List<Quest> getQuests() {
+        List<Quest> quests = new ArrayList<>();
+        return questDAO.loadAll();
+    }
+
+    private List<Card> getCards() {
+        List<Card> cards = new ArrayList<>();
+        return cardDAO.loadAll();
+    }
+
+    private List<Transaction> getTransactions() {
+        transactions = new ArrayList<>();
+        return transactionsDAO.loadAll();
     }
 
     private void showTransactions() {
-
+        transactions = getTransactions();
+        terminalView.displayCardTransactions(transactions);
     }
 
-    private void buyCard(Card card) {
+    private void buyCard(Student student) {
+        showAllCards();
+        cards = getCards();
+        int cardToBuyArrayIndex = terminalView.getOptionInput(cards.size()) - 1;
+        Card cardToBuy = cards.get(cardToBuyArrayIndex);
+        int cardToBuyDbIndex = cardToBuy.getId();
+        CardTransaction cardTransaction = new CardTransaction(cardToBuyDbIndex, student.getId(), cardToBuy.getCost());
+        transactionsDAO.addCardTransaction(cardTransaction);
+    }
 
+    private void submitQuest(Student student) {
+        showAllCards();
+        quests = getQuests();
+        int questToSubmitArrayIndex = terminalView.getOptionInput(quests.size()) - 1;
+        Card questToSubmit = cards.get(questToSubmitArrayIndex);
+        int questToSubmitDbIndex = questToSubmit.getId();
+        QuestTransaction questTransaction = new QuestTransaction(questToSubmitDbIndex, student.getId(), questToSubmit.getCost());
+        transactionsDAO.addQuestTransaction(questTransaction);
     }
 }
