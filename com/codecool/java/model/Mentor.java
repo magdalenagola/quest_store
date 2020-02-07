7f1051b18a9321aeb9969564a48b8dc44dde9a98
szczepanTopolski <szczepanPL678@gmail.com>
2020-02-07 11:35:57 +0100

    Overload mentors constructor

--- com/codecool/java/model/Mentor.java
+++ com/codecool/java/model/Mentor.java
@@ -8,13 +8,25 @@ public class Mentor extends User{
     private String password;
     private String name;
     private String surname;
+    private String primarySkill;
+    private boolean isActive;
 
-    public Mentor(int id,String login, String password,String name,String surname){
+    public Mentor(int id,String login, String password,String name,String surname,String primarySkill,boolean isActive){
         this.id = id;
         this.login = login;
         this.password = password;
         this.name = name;
         this.surname = surname;
+        this.primarySkill = primarySkill;
+        this.isActive = isActive;
+    }
+    public Mentor(String login, String password,String name,String surname,String primarySkill,boolean isActive){
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.primarySkill = primarySkill;
+        this.isActive = isActive;
     }
     @Override
     public void start() {
