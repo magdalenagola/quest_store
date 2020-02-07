867c035d03b95af4238d74fead908e6955292107
e-victoria <evictoria@protonmail.com>
2020-02-07 23:36:21 +0100

    Set fields and constructor in card and quest model

--- com/codecool/java/model/CardTransaction.java
+++ com/codecool/java/model/CardTransaction.java
@@ -1,4 +1,17 @@
 package codecool.java.model;
 
-public class CardTransaction {
+import java.util.Date;
+
+public class CardTransaction extends Transaction {
+    private int cardId;
+    private int userId;
+    private Date transactionDate;
+    int cost;
+
+    CardTransaction(int cardId, int userId, Date transactionDate, int cost) {
+        this.cardId = cardId;
+        this.userId = userId;
+        this.transactionDate = transactionDate;
+        this.cost = cost;
+    };
 }
--- com/codecool/java/model/QuestTransaction.java
+++ com/codecool/java/model/QuestTransaction.java
@@ -1,4 +1,17 @@
 package codecool.java.model;
 
-public class QuestTransaction {
+import java.util.Date;
+
+public class CardTransaction extends Transaction {
+    private int questId;
+    private int userId;
+    private Date transactionDate;
+    int cost;
+
+    CardTransaction(int questId, int userId, Date transactionDate, int cost) {
+        this.questId = questId;
+        this.userId = userId;
+        this.transactionDate = transactionDate;
+        this.cost = cost;
+    };
 }
