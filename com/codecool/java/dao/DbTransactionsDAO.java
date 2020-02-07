94a7a8be8febb5afde950b8ba376bcc9faf15aa6
Magdalena Gola <turcza.magdalena@gmail.com>
2020-02-07 19:58:07 +0100

    Add contructor

--- com/codecool/java/dao/DbTransactionsDAO.java
+++ com/codecool/java/dao/DbTransactionsDAO.java
@@ -1,16 +1,25 @@
 package codecool.java.dao;
 
+import codecool.java.model.BasicConnectionPool;
+
 import java.util.List;
 
 public class DbTransactionsDAO implements TransactionsDAO{
+    private BasicConnectionPool pool;
+
+    public DbTransactionsDAO(){
+        Class.forName("org.postgresql.Driver");
+        this.pool = BasicConnectionPool.create("jdbc:postgresql://ec2-176-34-237-141.eu-west-1.compute.amazonaws.com:5432/dbc5jifafq3j1h?sslmode=require",
+                "utiuhfgjckzuoq", "17954f632e3663cbadb55550dd636f4c3a645ade56c3342ee89f71fc732c9672");
+    }
 
     @Override
-    public List<Transactions> loadAllNotApproved() {
+    public List<Transaction> loadAllNotApproved() {
         return null;
     }
 
     @Override
-    public List<Transactions> displayAllTransactionsByStudent(Student student) {
+    public List<Transaction> displayAllTransactionsByStudent(Student student) {
         return null;
     }
 
@@ -36,6 +45,7 @@ public class DbTransactionsDAO implements TransactionsDAO{
 
     @Override
     public List loadAll() {
+
         return null;
     }
 
