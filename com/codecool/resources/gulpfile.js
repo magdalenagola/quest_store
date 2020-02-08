8f00f068695f1b633019f4cd5e44f3492e78c54b
e-victoria <evictoria@protonmail.com>
2020-02-08 20:36:08 +0100

    Fix mistake in gulp function

--- com/codecool/resources/gulpfile.js
+++ com/codecool/resources/gulpfile.js
@@ -7,7 +7,7 @@ var autoprefixer = require("autoprefixer");
 var del = require('del');
 
 gulp.task("css", function () {
-	return gulp.src(['src/css/*.css', 'src/css/blocks/*.css'])
+	return gulp.src(['src/css/*.css', 'src/css/blocks/*.css', '!src/css/style.css'])
 		.pipe(csso())
 		.pipe(postcss([
 			autoprefixer()
