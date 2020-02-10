3af16d8dd5e31e49f3099aca36c603a85fe74942
e-victoria <evictoria@protonmail.com>
2020-02-10 23:53:14 +0100

    Add salary to mentors in mobile version

--- com/codecool/resources/src/js/table_expander.js
+++ com/codecool/resources/src/js/table_expander.js
@@ -3,16 +3,17 @@ export default function expandTableContent() {
     const transtactionItemsContent = document.querySelectorAll('.transactions__content-wrapper');
     const mentorsItems = document.querySelectorAll('li.mentors__item');
     const mentorsItemsContent = document.querySelectorAll('.mentors__content-wrapper');
-
+    let mentorsSalary = document.querySelectorAll('p.mentors__salary');
     for (let i = 0; i < transactionItems.length; i++) {
         transactionItems[i].addEventListener('click', () => {
             transtactionItemsContent[i].classList.toggle('transactions__content-wrapper--opened');
         })
     }
     for (let i = 1; i < mentorsItems.length; i++) {
+        mentorsSalary[i-1].textContent += ' $';
         mentorsItems[i].addEventListener('click', (e) => {
             if(e.toElement.tagName == 'H3') {
-            mentorsItemsContent[i].classList.toggle('mentors__content-wrapper--opened');
+                mentorsItemsContent[i].classList.toggle('mentors__content-wrapper--opened');
             }
         })
     }
