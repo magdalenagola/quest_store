5687d91c0edcfcee042e536661faf66f7a9e6564
szczepanTopolski <szczepanPL678@gmail.com>
2020-02-10 16:06:21 +0100

    Mentor can rate not approved assignments

--- com/codecool/java/controller/MentorController.java
+++ com/codecool/java/controller/MentorController.java
@@ -1,17 +1,26 @@
 package codecool.java.controller;
 
+import codecool.java.dao.StudentDAO;
+import codecool.java.dao.TransactionsDAO;
 import codecool.java.model.Card;
 import codecool.java.model.Mentor;
 import codecool.java.model.Student;
 import codecool.java.view.Display;
 import codecool.java.view.TerminalView;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MentorController {
     Display view = new TerminalView();
-    StudentDAO studentDAO = new DBstudentDAO();
-    QuestDAO questDAO = new DBquestDAO();
-    CardDAO cardDAO = new DBcardDAO();
+    StudentDAO studentDAO = new DbstudentDAO();
+    QuestDAO questDAO = new DbquestDAO();
+    CardDAO cardDAO = new DbcardDAO();
+    TransactionsDAO transactionsDAO = new DbtransactionsDAO();
     public void run(){
         String[] options = {"Add new codecooler.","Add new quest.","Add new card","Update card",
         "Rate student's assigment","Display students statistics"};
@@ -40,17 +49,33 @@ public class MentorController {
     }
 
     private void displayStatistics() {
-        ArrayList<Student> students= studentDAO.loadAll();
+        List<Student> students= studentDAO.loadAll();
         for(Student student:students){
             view.displayMessage(student.toString());
-            view.displayMessage("Coins:"+String.valueOf(studentsDAO.getCoins(student)));
+            view.displayMessage("Coins:"+String.valueOf(studentDAO.getCoins(student)));
         }
     }
 
     private void rateAssigment() {
-        //TODO DISPLAY ONLY PENDINGS TRANSACTIONS
+        List<Transactions> notApprovedTransactions = transactionsDAO.loadAllNotApproved();
+        view.displayCardTransactions(notApprovedTransactions);
+        int studentTransactionIndex = view.getOptionInput(notApprovedTransactions.size());
+        QuestTransaction studentTransaction = notApprovedTransactions.get(studentTransactionIndex-1);
+        try {
+            studentTransaction.setTransactionDate(getTodayDate());
+            transactionsDAO.updateStudentQuest(studentTransaction);
+        } catch (ParseException e) {
+            view.displayErrorMessage(e);
+        }
     }
 
+    private Date getTodayDate() throws ParseException {
+        String datePattern = "MM-dd-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(datePattern);
+        String stringDate = simpleDateFormat.format(new Date());
+        Date todayDate = new SimpleDateFormat("MM-dd-yyyy").parse(stringDate);
+        return todayDate;
+    }
     private void updateCard() {
         ArrayList<Card> cards = cardDAO.loadAll();
         view.displayCards(cards);
