0e77ffa9ef8ee8754cfc68a8dd145c668b659ccd
Piterino <peter.lyskawa1995@gmail.com>
2020-02-13 10:55:30 +0100

    Adding Transaction

--- com/codecool/java/model/Transaction.java
+++ com/codecool/java/model/Transaction.java
@@ -1,4 +1,47 @@
 package codecool.java.model;
 
-public class Transaction {
+public abstract class Transaction {
+    private int itemId;
+    private int userId;
+    private String date;
+    private int cost;
+
+    public Transaction(int itemId, int userId, String date, int cost) {
+        this.itemId = itemId;
+        this.userId = userId;
+        this.date = date;
+        this.cost = cost;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
