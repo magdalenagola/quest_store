e8b1209174d83af8d18c4f7adbf0e7b70af83639
e-victoria <evictoria@protonmail.com>
2020-02-18 20:17:43 +0100

    Fix links, js

--- com/codecool/resources/src/js/index.js
+++ com/codecool/resources/src/js/index.js
@@ -3,10 +3,14 @@
 import toggleMenu from './menu.js';
 import expandTableContent from './table_expander.js';
 import AddMentorPopUpController from './AddMentorPopUpController.js';
+import EditMentorPopUpController from './MentorEditor.js';
 
 toggleMenu();
 expandTableContent();
 
 const addMentorPopUpController = new AddMentorPopUpController();
+const editMentorPopUpController = new EditMentorPopUpController();
 addMentorPopUpController.openAddMentorPopUp();
 addMentorPopUpController.closeAddMentorPopUp();
+editMentorPopUpController.openEditMentorPopUp();
+editMentorPopUpController.closeEditMentorPopUp();
