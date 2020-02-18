f1c09039d6e07ecdf8bb707cb80a083f397e9be4
Piterino <peter.lyskawa1995@gmail.com>
2020-02-18 15:52:11 +0100

    DbLogin now extends a single connection pool

--- com/codecool/java/dao/DbLoginDAO.java
+++ com/codecool/java/dao/DbLoginDAO.java
@@ -7,13 +7,10 @@ import codecool.java.model.UserFactory;
 import java.sql.*;
 
 
-public class DbLoginDAO implements LoginDao {
-    private BasicConnectionPool pool;
+public class DbLoginDAO extends DbConnectionDao implements LoginDao {
 
     public DbLoginDAO() throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        this.pool = BasicConnectionPool.create("jdbc:postgresql://ec2-176-34-237-141.eu-west-1.compute.amazonaws.com:5432/dbc5jifafq3j1h?sslmode=require",
-                "utiuhfgjckzuoq", "17954f632e3663cbadb55550dd636f4c3a645ade56c3342ee89f71fc732c9672");
+        super();
     }
 
     @Override
