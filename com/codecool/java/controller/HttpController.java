76d346657fe72871f74e6de79634999758953ceb
Magdalena Gola <turcza.magdalena@gmail.com>
2020-02-19 13:48:56 +0100

    Add mime types, static

---
+++ com/codecool/java/controller/HttpController.java
@@ -0,0 +1,18 @@
+package codecool.java.controller;
+
+import codecool.java.handler.StaticHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HttpController {
+        public void init() throws IOException {
+            int port = 8000;
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+           // server.createContext("/student", new StudentController());
+            server.createContext("/static", new StaticHandler());
+            server.setExecutor(null);
+            server.start();
+        }
+}
