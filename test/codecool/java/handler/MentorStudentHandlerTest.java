0ff3a9bccbed4d23780258252471ba486329e7d6
e-victoria <evictoria@protonmail.com>
2020-03-16 12:25:15 +0100

    Test create student from json

--- test/codecool/java/handler/MentorStudentHandlerTest.java
+++ test/codecool/java/handler/MentorStudentHandlerTest.java
@@ -1,23 +1,20 @@
 package codecool.java.handler;
 
-import codecool.java.dao.DbCardDAO;
 import codecool.java.dao.DbstudentDAO;
-import codecool.java.helper.HttpResponse;
 import codecool.java.model.Student;
 import com.google.gson.Gson;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
+import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 
-import java.io.IOException;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MentorStudentHandlerTest {
+
     @Test
-    void loadAllStudents() {
+    void ShouldLoadAllStudents() {
         DbstudentDAO mockStudentDAO = Mockito.mock(DbstudentDAO.class);
         Student student1 = new Student("alex", "123", "Alex", "Smith", true);
         Student student2 = new Student("john", "123", "John", "Milton", true);
@@ -26,5 +23,14 @@ class MentorStudentHandlerTest {
         String studentList = gson.toJson(mockStudentDAO.loadAll());
         String expected = "[{\"id\":0,\"login\":\"alex\",\"password\":\"123\",\"name\":\"Alex\",\"surname\":\"Smith\",\"isActive\":true},{\"id\":0,\"login\":\"john\",\"password\":\"123\",\"name\":\"John\",\"surname\":\"Milton\",\"isActive\":true}]";
         assertEquals(expected, studentList);
+    }
+
+    @Test
+    void shouldCreateStudentFromJson() {
+        String stringData = "{\"id\":0,\"login\":\"alex\",\"password\":\"123\",\"name\":\"Alex\",\"surname\":\"Smith\",\"isActive\":true}";
+        Gson gson = new Gson();
+        Student expected = gson.fromJson(stringData, Student.class);
+        Student actual = new Student(0, "alex", "123", "Alex", "Smith", true);
+        assertTrue(new ReflectionEquals(expected).matches(actual));
     }
 }
