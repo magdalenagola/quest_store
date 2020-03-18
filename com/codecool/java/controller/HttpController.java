458cb626c4456e25409cbc3e2fa8f75ca303318b
szczepanTopolski <szczepanPL678@gmail.com>
2020-03-18 09:46:18 +0100

    Use created helpers instead of new instances

--- com/codecool/java/controller/HttpController.java
+++ com/codecool/java/controller/HttpController.java
@@ -25,7 +25,7 @@ public class HttpController {
             server.createContext("/static", new StaticHandler());
             server.createContext("/student/transactions", new TransactionsHandler(new DbstudentDAO(), new DbTransactionsDAO()));
             server.createContext("/student/inventory", new InventoryHandler(new DbstudentDAO(), new DbCardDAO()));
-            server.createContext("/mentor/quests", new MentorQuestHandler(new DbQuestDAO(), new CookieHelper(), new HttpResponse()));
+            server.createContext("/mentor/quests", new MentorQuestHandler(new DbQuestDAO(), cookieHelper, httpResponse));
             server.createContext("/student/quests", new StudentQuestHandler());
             server.createContext("/manager/mentor", new ManagerMentorsHandler());
             server.setExecutor(null);
