009f1c72a74a749ed9e597dd2e14d6e74b2e5740
Magdalena Gola <turcza.magdalena@gmail.com>
2020-03-19 11:46:48 +0100

    Refactor test shouldReturnTransactionsForGivenStudent

--- test/codecool/java/handler/TransactionsHandlerTest.java
+++ test/codecool/java/handler/TransactionsHandlerTest.java
@@ -3,6 +3,8 @@ package codecool.java.handler;
 import codecool.java.dao.DbTransactionsDAO;
 import codecool.java.dao.DbstudentDAO;
 import codecool.java.model.*;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Date;
@@ -16,21 +18,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class TransactionsHandlerTest {
+    DbTransactionsDAO mockTransactionsDAO;
+    TransactionsHandler transactionsHandler;
+
+    @BeforeEach
+    void setUp(){
+        mockTransactionsDAO = mock(DbTransactionsDAO.class);
+        transactionsHandler = new TransactionsHandler(mock(DbstudentDAO.class),mockTransactionsDAO);
+    }
 
     @Test
     public void shouldReturnTransactionsForGivenStudent(){
-        DbTransactionsDAO mockTransactionsDAO = mock(DbTransactionsDAO.class);
-        TransactionsHandler transactionsHandler = new TransactionsHandler(mock(DbstudentDAO.class),mockTransactionsDAO);
         Student dummyStudent = mock(Student.class);
+        when(mockTransactionsDAO.displayAllTransactionsByStudent(dummyStudent)).thenReturn(createSampleMapOfStudentQuests());
+        String expected = "{\"Quests\":[{\"item\":{\"category\":\"category\",\"id\":1,\"title\":\"title\",\"description\":\"description\",\"image\":\"image\",\"isActive\":true,\"cost\":22},\"userId\":1,\"date\":" +
+                createSampleGsonDate() +
+                ",\"cost\":22}]}";
+        String actual = transactionsHandler.getStudentTransactions(dummyStudent);
+        assertEquals(expected,actual);
+    }
+
+    private Map<String, List<Transaction>> createSampleMapOfStudentQuests(){
         Map<String, List<Transaction>> quests = new HashMap<>();
         Item quest = new Quest(1, "title", "description", "image", true, 22, "category");
         Transaction questTransaction = new QuestTransaction(quest, 1, new Date(2222), 22);
         List<Transaction> transactions = new ArrayList<>();
         transactions.add(questTransaction);
         quests.put("Quests",transactions);
-        when(mockTransactionsDAO.displayAllTransactionsByStudent(dummyStudent)).thenReturn(quests);
-        String expected = "{\"Quests\":[{\"item\":{\"category\":\"category\",\"id\":1,\"title\":\"title\",\"description\":\"description\",\"image\":\"image\",\"isActive\":true,\"cost\":22},\"userId\":1,\"date\":\"sty 1, 1970\",\"cost\":22}]}";
-        String actual = transactionsHandler.getStudentTransactions(dummyStudent);
-        assertEquals(expected,actual);
+        return quests;
+    }
+
+    private String createSampleGsonDate(){
+        Date date = new Date(2222);
+        Gson gson = new Gson();
+        return gson.toJson(date);
     }
 }
