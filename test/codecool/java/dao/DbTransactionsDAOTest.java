c573766313bec621d0a50fe0a82a16a1df500d5c
Magdalena Gola <turcza.magdalena@gmail.com>
2020-03-19 14:08:36 +0100

    Add transactions dao test file

---
+++ test/codecool/java/dao/DbTransactionsDAOTest.java
@@ -0,0 +1,48 @@
+package codecool.java.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DbTransactionsDAOTest {
+
+    @Test
+    void loadAllNotApproved() {
+    }
+
+    @Test
+    void displayAllTransactionsByStudent() {
+    }
+
+    @Test
+    void getCardTransactionsByStudent() {
+    }
+
+    @Test
+    void getQuestTransactionsByStudent() {
+    }
+
+    @Test
+    void addCardTransaction() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void loadAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void disable() {
+    }
+
+    @Test
+    void activate() {
+    }
+}
